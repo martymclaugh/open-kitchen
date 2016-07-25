0c3e7205383594cdd5a78c126d735fa4779a7700
Marty McLaughlin <martymclaugh@gmail.com>
2016-07-25 16:19:48 -0700

    add login functionality and start on search

--- app/controllers/indexes_controller.rb
+++ app/controllers/indexes_controller.rb
@@ -4,5 +4,9 @@ end
 
 get '/search' do
   client = Yelp::Client.new({ consumer_key: ENV['CONSUMER_KEY'], consumer_secret: ENV['CONSUMER_SECRET'], token: ENV['TOKEN'], token_secret: ENV['TOKEN_SECRET']})
- p client.search('San Francisco', { term: 'food', limit: 1 })
+ p client.search(params[:zipcode], { term: params[:name], limit: 1 })
+end
+
+post '/search' do
+  
 end
