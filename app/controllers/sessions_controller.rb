94d140af294dfb53f874cf384fe7b66f1d024266
Marty McLaughlin <martymclaugh@gmail.com>
2016-07-25 18:09:07 -0700

    have logout button show when logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,13 @@
 post '/register' do
-  p params
   if params[:password1] == params[:password2]
     user = User.create(username: params[:username], full_name: params[:full_name], email: params[:email], password: params[:password2])
     if user
       redirect '/'
     end
   else
-    if params[:password1] == params[:password2]
-      @register_error = "*Passwords don't match"
-    end
-    redirect '/'
+    @register_error = "*Passwords don't match"
+    p @register_error
+    erb :index
   end
 end
 
@@ -20,12 +18,14 @@ end
 
 post '/login' do
   user = User.authenticate(params[:username], params[:password])
+
   if user
     login(user)
-    redirect '/profile'
+    p current_user
+    redirect '/'
   else
     @login_error = "*Wrong username or password"
-    p @login_error
-    redirect '/'
+    @login_error
+    erb :index
   end
 end
